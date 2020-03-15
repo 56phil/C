676b312cfa35a72191461b52a0a36368f86dac4f
Phil Huffman <56phil@gmail.com>
2020-03-15 17:06:54 -0500

    Fixes use of replacement character.

--- translate.py
+++ translate.py
@@ -1,35 +1,40 @@
 #!/usr/bin/env python3
-import sys
 import argparse
 
 '''
 translate.py
 
-This is a python program that can read a text file (fn_i) and write a text file
-with named fs_o. It reads the very first data set, determines where the protein
-sequence is in the count. Then in every subsequent data set the program will
-only return data that is related to that count. This will remove data that does
-that does not precisely align with Drosophila sequence positions  (wether they
-are dash or letter). The output file will have the same number of data sets. The
-sequence of the first data set is unchanged. In the remaining data sets, each
-character of the sequence is unchanged if the corresponding character of the
-first sequence is a letter. Otherwise it is replaced with a '+'.
+This is a python program that reads a text file and writes a
+text file with a name derived from the name of the input file (e.g. d.txt ->
+d.OUT.txt). It reads the first data set, determines where the protein sequence
+is in the count. Then, in every subsequent data set, the program will only
+return data that is related to that count. This will replace data that does
+that does not precisely align with sequence positions from the first data set
+(whether they are a dash or a letter). The output file will have the same
+number of data sets  as the input file. The protiens sequence of the first
+data set is unchanged. In the remaining data sets, each character of the
+sequence is unchanged if the corresponding character of the first sequence is
+a letter. Otherwise, it is replaced with the specified replacement character.
+If none is specified, the default '+' will be used.
 
 Input file format:
     '>' + other species information + '[' + full species name + ']' +
     protiens sequence
 
 Output file format:
-    '>' + full species name + '\n' + protiens sequence + '\n\n'
+    '>' + full species name + '\n' + protiens sequence + '\n'
 
 Usage:
     $./translate.py <FILE NAME> [LENGTH OF SEQUENCE LINES]
+    [REPLACEMENT CHARACTER]
 
 Examples:
-    $./translate.py some_file.txt 80
+    $./translate.py some_file.txt 42 .
+    $./translate.py some_file.txt 123 \?
     $./translate.py some_file.txt
 
     The line length parameter is optional and defaults to 60.
+    The default replacement character is '+'.
 '''
 
 
@@ -43,20 +48,21 @@ def get_fsn_and_sequence(str_i):
     return ['>' + str_i[start+1:end], str_i[end+1:].replace('\n', '')]
 
 
-def format_sequence(template, sequence):
-    """ Places a '+' everywhere there is a False in the template list
+def format_sequence(template, sequence, repl_char):
+    """ Places the replacement character everywhere there is a False in the
+        template list
         input: string
         returns: string
     """
-    return ''.join([c if b else '+' for b, c in zip(template, sequence)])
+    return ''.join([c if b else repl_char for b, c in zip(template, sequence)])
 
 
-def read_file(fn_i):
-    """ Builds the raw data list by reading the text file named by fn_i
+def read_file(file_name):
+    """ Builds the raw data list by reading the text file named by file_name
         input: None
         returns: list
     """
-    with open(fn_i) as f:
+    with open(file_name) as f:
         ds = f.readlines()
     raw_list = ''.join(ds).split('>')[1:]
     lc = len(raw_list)
@@ -75,7 +81,7 @@ def parse_raw_data(raw_list):
     return [get_fsn_and_sequence(raw_dataset) for raw_dataset in raw_list]
 
 
-def format_parsed_data(data):
+def format_parsed_data(data, repl_char):
     """ Builds the template list
         Replaces each character in the sequence with '+' where the corresponding
         value in the template is False.
@@ -86,18 +92,18 @@ def format_parsed_data(data):
     sequence = data[0][1]
     template = [True if c.isalpha() else False for c in sequence]
     for i, datum in enumerate(data):
-        data[i][1] = format_sequence(template, datum[1])
+        data[i][1] = format_sequence(template, datum[1], repl_char)
     data[0][1] = sequence
     return data
 
 
-def write_file(fn_i, data, line_size):
-    """ Writes the results to a text file using a name based on fn_i
+def write_file(file_name, data, line_size):
+    """ Writes the results to a text file using a name based on file_name
         input: string, list
         returns: None
     """
-    pos = fn_i.rfind('.')
-    fn_o = fn_i[:pos] + '.OUT' + fn_i[pos:]
+    pos = file_name.rfind('.')
+    fn_o = file_name[:pos] + '.OUT' + file_name[pos:]
     f = open(fn_o, "w")
     for fsn, sequence in data:
         f.write(fsn + '\n')
@@ -112,8 +118,13 @@ if __name__ == '__main__':
                         help='Name of input file.')
     parser.add_argument('line_size', type=int, nargs='?', default=60,
                         help='Number of characters in a sequence line.')
+    parser.add_argument('replacement_character', type=str,
+                        nargs='?', default='+',
+                        help='Character used to mark openings.')
     args = parser.parse_args()
 
+    repl_char = args.replacement_character[0]
+
     write_file(args.file_name,
-        format_parsed_data(parse_raw_data(read_file(args.file_name))),
-        args.line_size)
+        format_parsed_data(parse_raw_data(read_file(args.file_name)),
+        repl_char), args.line_size)
